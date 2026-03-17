cba32fd99418d6145802094c7778414f23f1a3ee
B Rai <bishwarrai2018@gmail.com>
2026-03-17 16:53:28 -0700

    Add methodology description to DID script

--- code/did_analysis.py
+++ code/did_analysis.py
@@ -1,3 +1,9 @@
+# did_analysis.py
+# Computes the difference-in-differences estimate for the eBay paid search experiment.
+# Method: Compare pre-post log revenue changes between treatment and control DMAs.
+# Reference: Blake et al. (2014), Taddy Ch. 5
+
+
 # -- Step 1 ---
 import pandas as pd
 import numpy as np
